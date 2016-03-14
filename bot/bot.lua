e44fa064fc28efe72914e284405cf7902034d122
khaneshir <khanehshir12@gmail.com>
2016-03-14 23:42:26 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {172249322},
+    sudo_users = {138583448,147368581},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
